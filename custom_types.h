a3bc8303e13d612298479e2cf3a22ae5ec239223
Sk Golam Kuddus <143329516+Kuddus24@users.noreply.github.com>
2024-09-16 18:08:27 +0530

    Update custom_types.h

--- custom_types.h
+++ custom_types.h
@@ -1,8 +1,11 @@
 #ifndef CUSTOM_TYPES_H
 #define CUSTOM_TYPES_H
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
 typedef unsigned char u8;
 typedef unsigned short u16; // 16-bit unsigned data type
 typedef unsigned int u32;
-#endif 
-
+#endif
